07f49c04542f4eb3814900245989e9faae7110dd
Benjamen Keroack <benjamen@dollarshaveclub.com>
2020-01-03 10:36:18 -0800

    use secrets engine v1

--- run.sh
+++ run.sh
@@ -29,6 +29,10 @@ vault server \
 
 sleep 1 # wait for Vault to come up
 
+# use secrets engine v1
+vault secrets disable secret
+vault secrets enable -version=1 -path=secret -description='local secrets' kv
+
 # parse JSON array, populate Vault
 if [[ -f "$VAULT_SECRETS_FILE" ]]; then
   for path in $(jq -r 'keys[]' < "$VAULT_SECRETS_FILE"); do
